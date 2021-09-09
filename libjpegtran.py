3e2d1f6df16317b4856a30d98f4d6e39f602c8af
Sirech-hub <76720138+Sirech-hub@users.noreply.github.com>
2021-09-09 09:55:09 +0300

    Update libjpegtran.py

--- libjpegtran.py
+++ libjpegtran.py
@@ -3,18 +3,16 @@ from os.path import isfile
 from os.path import join as joinpath
 import subprocess
 
-# прописываем имя исходной папки 
+
 mypath = "images"
 
-# Перебираем каждый файл в папке
 for i in listdir(mypath):
-    # Проверяем не папка ли этот файл
+   
     if isfile(joinpath(mypath,i)):
         print (i)
-        # Формируем команду для jpegtran
+     
         cmd = 'jpegtran -progressive -copy none -optimize -outfile optimized/' +i+'  images/'+ i
         PIPE = subprocess.PIPE
-        # Выполняем команду 
+       
         p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
 
-#jpegtran читаем мануал 
